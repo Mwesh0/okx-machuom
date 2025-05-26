e13604c4e19d507de6e38e8e4387fa0c625a048a
mwesh0 <mwemadenis2@gmail.com>
2025-05-26 14:13:46 +0300

    CMA updates stocks

--- CMA/CMA.js
+++ CMA/CMA.js
@@ -1,11 +1,16 @@
 import { stocks } from '../machuom/machuom-stocks.js';
+//onclicks
+document.querySelector('.add-stocks-button').addEventListener('click',updateStockQuantity);
 
-function updateStockQuantity(companyName, addedAmount) {
+function updateStockQuantity() {
+  const companyName = document.querySelector('.company-name-input').value
+  const addedAmount =  parseFloat(document.querySelector('.added-amount-input').value);
   const stock = stocks.find(s => s.name === companyName);
   if (!stock) {
     console.error(`Stock ${companyName} not found.`);
     return;
+  } else {
+    stock.quantity += addedAmount;
+    console.log(`Updated ${companyName} quantity: ${addedAmount} shares added.`);
   }
-  stock.quantity += addedAmount;
-  console.log(`Updated ${companyName} quantity: ${stock.quantity}`);
 }
